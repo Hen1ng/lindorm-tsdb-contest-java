ea986539f7e73c80cc84765e1344deef61f6e23b
hening <ninghe.hn@alibaba-inc.com>
2023-09-13 20:34:48 +0800

    update

--- src/main/java/com/alibaba/lindorm/contest/example/QueryTest.java
+++ src/main/java/com/alibaba/lindorm/contest/example/QueryTest.java
@@ -81,7 +81,7 @@ public class QueryTest {
                     columns.put(key, new ColumnValue.StringColumn(buffer));
                 }
             }
-
+//
 //            Schema schema = new Schema(columnTypeMap);
 //            tsdbEngineSample.connect();
 //            tsdbEngineSample.createTable("haha", schema);
@@ -101,7 +101,7 @@ public class QueryTest {
 //                            rowList.add(new Row( vin, atomicLong.getAndIncrement() * 1000, columns));
 //                        }
 //                        try {
-//                            tsdbEngineSample.upsert(new WriteRequest("test", rowList));
+//                            tsdbEngineSample.write(new WriteRequest("test", rowList));
 ////
 //                        } catch (Exception e) {
 //
@@ -118,7 +118,7 @@ public class QueryTest {
 //            tsdbEngineSample.shutdown();
             tsdbEngineSample.connect();
             List<Vin> list = new ArrayList<>();
-            list.add(new Vin("3TZgBg7DMD2awLDNC".getBytes(StandardCharsets.UTF_8)));
+            list.add(new Vin("Yk6Hml0zPusCR6c3B".getBytes(StandardCharsets.UTF_8)));
             Set<String> requestedColumns = new HashSet<>();
             requestedColumns.add("5String543210");
             requestedColumns.add("3String3210");
@@ -132,11 +132,11 @@ public class QueryTest {
 
             final LatestQueryRequest latestQueryRequest = new LatestQueryRequest("", list, requestedColumns);
             final ArrayList<Row> rows = tsdbEngineSample.executeLatestQuery(latestQueryRequest);
-            final TimeRangeQueryRequest timeRangeQueryRequest = new TimeRangeQueryRequest("", new Vin("3TZgBg7DMD2awLDNC".getBytes(StandardCharsets.UTF_8)), requestedColumns, 0, Long.MAX_VALUE);
+            final TimeRangeQueryRequest timeRangeQueryRequest = new TimeRangeQueryRequest("", new Vin("Yk6Hml0zPusCR6c3B".getBytes(StandardCharsets.UTF_8)), requestedColumns, 11000, 21000);
             ArrayList<Row> rowArrayList = tsdbEngineSample.executeTimeRangeQuery(timeRangeQueryRequest);
-            final TimeRangeAggregationRequest timeRangeAggregationRequest = new TimeRangeAggregationRequest("", new Vin("3TZgBg7DMD2awLDNC".getBytes(StandardCharsets.UTF_8)), "7double", 0, Long.MAX_VALUE, Aggregator.MAX);
+            final TimeRangeAggregationRequest timeRangeAggregationRequest = new TimeRangeAggregationRequest("", new Vin("Yk6Hml0zPusCR6c3B".getBytes(StandardCharsets.UTF_8)), "7double", 11000, 21000, Aggregator.MAX);
             rowArrayList = tsdbEngineSample.executeAggregateQuery(timeRangeAggregationRequest);
-            final TimeRangeDownsampleRequest timeRangeDownsampleRequest = new TimeRangeDownsampleRequest("", new Vin("3TZgBg7DMD2awLDNC".getBytes(StandardCharsets.UTF_8)), "7double", 9120000L, 10923000L, Aggregator.AVG, 1000L, new CompareExpression(new ColumnValue.DoubleFloatColumn(0.7d), CompareExpression.CompareOp.EQUAL));
+            final TimeRangeDownsampleRequest timeRangeDownsampleRequest = new TimeRangeDownsampleRequest("", new Vin("Yk6Hml0zPusCR6c3B".getBytes(StandardCharsets.UTF_8)), "7double", 9120000L, 10923000L, Aggregator.AVG, 1000L, new CompareExpression(new ColumnValue.DoubleFloatColumn(0.7d), CompareExpression.CompareOp.EQUAL));
             rowArrayList = tsdbEngineSample.executeDownsampleQuery(timeRangeDownsampleRequest);
             System.out.println(1);
             tsdbEngineSample.shutdown();
